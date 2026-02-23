de7e4729a9befe5197941e3618d8377706319c4a
martiroman <martinnahuel.roman@proton.me>
2026-02-23 18:39:53 -0300

    modificacion script

--- assets/js/type_command.js
+++ assets/js/type_command.js
@@ -2,8 +2,7 @@
   const cmdSpan = cursor?.closest('.terminal-line')?.querySelector('.cmd');
 
   document.querySelectorAll('.btn').forEach(btn => {
-    const label = btn.textContent.trim().toLowerCase().replace(/\s+/g, '-');
-
+    const label = btn.dataset.cmd || btn.textContent.trim().toLowerCase().replace(/\s+/g, '-');
     btn.addEventListener('mouseenter', () => {
       if (cmdSpan) cmdSpan.innerHTML = ` ./${label}.sh <span class="terminal-cursor"></span>`;
     });
